a445d09207a31f9cb63b442afdcefa6a84ffb514
TazmanS <TazmanS123@gmail.com>
2025-01-08 16:48:22 +0200

    test jenkins v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import FullLayout from "@/layouts/FullLayout";
 export default function Home() {
   return (
     <FullLayout>
-      <div>Home version 1</div>
+      <div>Home version 2</div>
     </FullLayout>
   );
 }
